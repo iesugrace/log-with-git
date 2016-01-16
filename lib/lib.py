1c364e2e1af2a253ebba8200c8d77ce9680cc2d3
Joshua Chen <iesugrace@gmail.com>
2016-01-16 23:12:07 +0800

    Move some static methods to lib module

--- lib/lib.py
+++ lib/lib.py
@@ -1,4 +1,5 @@
 from subprocess import Popen, PIPE
+import base64
 
 class InvalidTimeException(Exception): pass
 
@@ -13,6 +14,46 @@ def get_status_byte_output(cmd):
     pStat   = (stat == 0)
     res     = (pStat, stdout, stderr)
     return res
+
+
+def isBinary(data):
+    """ Deem to be binary data if failed to decode
+    """
+    try:
+        data.decode()
+    except UnicodeDecodeError:
+        return True
+    else:
+        return False
+
+
+def b64encode(iData, lineLen=64):
+    """ Take the data which is a bytes,
+    encode it with base64, split the resulting
+    text into lines of 'lineLen', decode each
+    line (it was a bytes) to str, concatenate
+    them and return them as a whole long line.
+    """
+    lineLen = lineLen // 4 * 4  # make it times of 4
+    oData   = base64.b64encode(iData)
+    res     = []
+    while oData:
+        line, oData = oData[:lineLen], oData[lineLen:]
+        res.append(line.decode())
+    return '\n'.join(res)
+
+
+def b64decode(iData):
+    """ Take the str, decode it into binary
+    """
+    iData = iData.replace('\n', '')
+    return base64.b64decode(iData)
+
+
+def binToAsc(binData):
+    """ Convert the binary data into ASCII form
+    """
+    return b64encode(binData)
 
 
 class Pager:
