835ec69830179e254e9052ea4d2fecd1e554f767
Joshua Chen <iesugrace@gmail.com>
2016-01-27 18:59:59 +0800

    Factorize the config module

--- config.py
+++ config.py
@@ -2,21 +2,30 @@ import os
 
 class Config:
     """ Process and store the user settings
+
+    Subclass can redefine the attribute 'fileName'
+    and the method 'validate' to suite the needs.
     """
-    defaultConfigName = '.logrc'
+
+    fileName = '.logrc'
 
     def __init__(self, path=None):
-        """ Rule for the dataDir:
-        1. exists and is a directory, or
-        2. not yet exists
-        """
         if not path:
-            path = os.path.join(os.getenv('HOME'), self.defaultConfigName)
+            path = os.path.join(os.getenv('HOME'), self.fileName)
         code       = open(path).read()
         configs    = {}
         exec(code, configs)
         configs    = {k: v for k, v in configs.items() if k[0] != '_'}
         self.data  = configs
+
+    def validate(self):
+        """ Validate the config items
+        
+        Rule for the dataDir:
+        1. exists and is a directory, or
+        2. not yet exists
+        """
+        configs    = self.data
         dataDir    = configs.get('dataDir')
         dataDirSet = False
         if dataDir and (
